ba4257e8f259bacd4c791f0ca31d030260fd7c3c
arini nur rohmah <aini.lini@ymail.com>
2022-12-06 12:22:20 +0100

    Delete target_system directory

--- target_system/components/component/apps/g4t1.cpp
+++
@@ -1,6 +0,0 @@
-#include "component/g4t1/G4T1.hpp"
-
-int32_t main(int32_t argc, char **argv) {
-    G4T1 g4t1(argc, argv, "centralhub_vital");
-    return g4t1.run();
-}
--- target_system/components/component/apps/g5w1_3.cpp
+++
@@ -1,6 +0,0 @@
-#include "component/g5w1_3/G5W1_3.hpp"
-
-int32_t main(int32_t argc, char **argv) {
-    G5W1_3 g5w1_3(argc, argv, "hygrometer");
-    return g5w1_3.run();
-}
