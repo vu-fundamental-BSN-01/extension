de788b8b84363d052310491ea60b8cd6afaf2888
Elliot1235 <117409737+Elliot1235@users.noreply.github.com>
2022-12-19 21:15:05 +0100

    Create CentralHubWeather.hpp

---
+++ bsn-master/src/sa-bsn/target_system/components/component/include/component/CentralHubWeather.hpp
@@ -0,0 +1,53 @@
+#ifndef CENTRALHUBWEATHER_HPP
+#define CENTRALWEATHERHUBWEATHER_HPP
+
+#include <stdio.h> 
+#include <string>
+#include <numeric>
+
+#include "archlib/target_system/Component.hpp"
+#include "archlib/AdaptationCommand.h"  
+#include "messages/SensorData.h"
+
+#include "libbsn/resource/Battery.hpp"
+#include "libbsn/utils/utils.hpp"
+
+
+class CentralHubWeather : public arch::target_system::Component {
+
+    public:
+		CentralHubWeather(int &argc, char **argv, const std::string &name, const bool &active, const bsn::resource::Battery &battery);
+    	~CentralHub();
+
+	private:
+    	CentralHubWeather &operator=(const CentralHubWeather &);
+
+  	public:
+        virtual void setUp() = 0;
+    	virtual void tearDown() = 0;
+        virtual int32_t run();
+		void body();
+        void apply_noise();
+
+        void reconfigure(const archlib::AdaptationCommand::ConstPtr& msg);
+
+        virtual void collect(const messages::SensorData::ConstPtr& sensor_data) = 0;
+        virtual void process() = 0;
+        virtual void transfer() = 0;
+
+    private:
+        bool isActive();
+        void turnOn();
+        void turnOff();
+        void recharge();
+
+    protected:
+		bool active;
+        int max_size;
+        int total_buffer_size;
+        std::array<int, 6> buffer_size;
+		bsn::resource::Battery battery;
+        std::vector<std::list<double>> data_buffer;
+};
+
+#endif 
