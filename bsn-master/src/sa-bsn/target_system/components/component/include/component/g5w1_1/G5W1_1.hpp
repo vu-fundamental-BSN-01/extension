002f227a9fd74e40c1843deea13967be9d9d279d
Elliot1235 <117409737+Elliot1235@users.noreply.github.com>
2022-12-19 22:41:19 +0100

    Update G5W1_1.hpp

---
+++ bsn-master/src/sa-bsn/target_system/components/component/include/component/g5w1_1/G5W1_1.hpp
@@ -0,0 +1,56 @@
+#ifndef G5W1_1_HPP
+#define G5W1_1_HPP
+
+#include <string>
+#include <exception>
+
+#include "ros/ros.h"
+
+#include "libbsn/range/Range.hpp"
+#include "libbsn/resource/Battery.hpp"
+#include "libbsn/generator/Markov.hpp"
+#include "libbsn/generator/DataGenerator.hpp"
+#include "libbsn/filters/MovingAverage.hpp"
+#include "libbsn/utils/utils.hpp"
+#include "libbsn/configuration/SensorConfiguration.hpp"
+
+#include "component/Sensor.hpp"
+
+#include "messages/SensorData.h"
+#include "services/PatientData.h"
+
+class G5W1_1 : public Sensor {
+    	
+  	public:
+    	G5W1_1(int &argc, char **argv, const std::string &name);
+    	virtual ~G5W1_1();
+
+	private:
+      	G5W1_1(const G5W1_1 &);
+    	G3T1_3 &operator=(const G5W1_1 &);
+
+		std::string label(double &risk);
+    
+	public:
+		virtual void setUp();
+    	virtual void tearDown();
+
+        double collect();
+        double process(const double &data);
+        void transfer(const double &data);
+
+  	private:
+		bsn::generator::Markov markov;
+		bsn::generator::DataGenerator dataGenerator;		
+		bsn::filters::MovingAverage filter;
+		bsn::configuration::SensorConfiguration sensorConfig;
+
+		ros::NodeHandle handle;
+		ros::Publisher data_pub;
+		ros::ServiceClient client;	
+
+		double collected_risk;
+
+};
+
+#endif 
